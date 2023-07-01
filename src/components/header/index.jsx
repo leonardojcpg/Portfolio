211dd2e96d5558ca969ea855e60a7a974faf5d9a
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-07-01 01:15:39 -0300

    feat: applying material ui

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,19 +1,65 @@
-import {
-  HeaderContainer,
-  NavLinks,
-  NavButtons,
-  NavButtonContact
-} from "./style.js";
+import { Container } from "@mui/material";
+import { HeaderContainer, NavLinks } from "./style.js";
+import Button from "@mui/material/Button";
 
 export const Header = () => {
   return (
     <HeaderContainer>
-        <NavLinks>
-          <NavButtons href="/">Home</NavButtons>
-          <NavButtons href="/techs">Techs</NavButtons>
-          <NavButtons href="/projects">Projects</NavButtons>
-          <NavButtonContact>Contact</NavButtonContact>
-        </NavLinks>
+      <NavLinks>
+        <Button
+          sx={{
+            color: "#141414",
+            "&:hover": {
+              backgroundColor: "#dfdeda",
+              borderColor: "#5d740c",
+              color: "#5d740c",
+            },
+          }}
+        >
+          About
+        </Button>
+        <Button
+          sx={{
+            color: "#141414",
+            "&:hover": {
+              backgroundColor: "#dfdeda",
+              borderColor: "#5d740c",
+              color: "#5d740c",
+            },
+          }}
+        >
+          Techs
+        </Button>
+        <Button
+          sx={{
+            color: "#141414",
+            "&:hover": {
+              backgroundColor: "#dfdeda",
+              borderColor: "#5d740c",
+              color: "#5d740c",
+            },
+          }}
+        >
+          Projects
+        </Button>
+        <Container sx={{ display: "flex", marginLeft: "80%" }}>
+          <Button
+            variant="outlined"
+            sx={{
+              backgroundColor: "#5d740c",
+              color: "#dfdeda",
+              borderColor: "#5d740c",
+              "&:hover": {
+                backgroundColor: "#dfdeda",
+                borderColor: "#5d740c",
+                color: "#5d740c",
+              },
+            }}
+          >
+            Contact
+          </Button>
+        </Container>
+      </NavLinks>
     </HeaderContainer>
   );
 };
--- src/components/header/style.js
+++ src/components/header/style.js
@@ -13,34 +13,15 @@ export const NavLinks = styled.ul`
   display: flex;
   gap: 5rem;
   align-items: center;
+
 `;
 
 export const NavButtons = styled.a`
   cursor: pointer;
   text-decoration: none;
-  align-items: center;
-  justify-content: center;
   &:hover {
-    color: ${props => props.theme.buttonColor};
+    color: ${props => props.theme.buttonColor}; 
   }
 `;
 
-export const NavButtonContact = styled.button`
-    margin-left: 90%;
-    font-family: "Roboto", sans-serif;
-    font-weight: 400;
-    color: ${props => props.theme.backgroundColor};
-    background-color: ${props => props.theme.buttonColor};
-    height: 2.75rem;
-    padding: 0.7rem;
-    border: none;
-    border-radius: 0.5rem;
 
-    &:hover {
-    background-color: ${props => props.theme.buttonHover};
-    color: ${props => props.theme.fontColor};
-    transition: background-color 0.2s ease;
-  }
-
-    
-`
